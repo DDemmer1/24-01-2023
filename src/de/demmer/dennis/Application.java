6dac23503202ed7e32c39f39e1d7e8eb19bfa6b3
Unknown <Unknown@DESKTOP-4KSK0J5.fritz.box>
2023-01-24 09:03:07 +0100

    initial commit

--- src/de/demmer/dennis/Application.java
+++ src/de/demmer/dennis/Application.java
@@ -1,9 +1,6 @@
 package de.demmer.dennis;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+
 
 public class Application {
 	
